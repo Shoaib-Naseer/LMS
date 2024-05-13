1228c28f7e536b6169469bc31fdc0d179fde85e9
Shoaib Naseer <shoaibnaseer1111@gmail.com>
2024-05-14 02:02:59 +0500

    feat: Implemented responsive UI of the Login page

--- assessment-test/src/main.ts
+++ assessment-test/src/main.ts
@@ -2,4 +2,12 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+
+import router from "./router";
+
+import { BiEye, BiEyeSlash } from "oh-vue-icons/icons";
+
+addIcons(BiEye, BiEyeSlash);
+
+createApp(App).use(router).component("VIcon", OhVueIcon).mount("#app");
---
+++ assessment-test/src/views/Login.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="container max-w-[min(90%,768px)]">
+    <img src="/logo.webp" alt="Takmil Foundation" class="" />
+    <h1>TAKMIl Dashboard</h1>
+    <h3>Signin to continue</h3>
+  </section>
+</template>
+<script setup lang="ts"></script>
