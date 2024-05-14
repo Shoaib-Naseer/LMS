04f7cf69e07215eee9aca31276d6646aa9506fbf
Shoaib Naseer <shoaibnaseer1111@gmail.com>
2024-05-14 23:14:40 +0500

    Adding Routes Guard

--- assessment-test/src/router/index.ts
+++ assessment-test/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/Login.vue";
+import { getAccessToken } from "../helpers";
 
 const routes = [
   {
@@ -12,6 +13,21 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = getAccessToken() || false;
+  const nonAuthPagesNames = ["Login"];
+
+  if (!isAuthenticated && !nonAuthPagesNames.includes(to.name as any)) {
+    router.replace({ name: "Login" });
+  } else if (isAuthenticated && nonAuthPagesNames.includes(to.name as any)) {
+    next({ path: "/home" });
+  } else if (isAuthenticated && to.path === "/") {
+    next({ path: "/home" });
+  } else {
+    next();
+  }
 });
 
 export default router;
